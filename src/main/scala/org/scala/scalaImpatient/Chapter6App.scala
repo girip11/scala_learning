05015bc03ef7bfb9f938dcd91f88c16605e45dce
girish pasupathy <girishpasupathy@gmail.com>
2020-06-11 15:27:19 +0530

    Solutions to chapter 6 exercises

---
+++ src/main/scala/org/scala/scalaImpatient/Chapter6App.scala
@@ -0,0 +1,7 @@
+//package org.scala.scalaImpatient
+
+//Run in the terminal using the below command
+// scala src/main/scala/org/scala/scalaImpatient/Chapter6App.scala Hello world
+object Chapter6App extends App {
+  println(args.reverse.mkString(" "))
+}
